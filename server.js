b42dc29094e738703d6e83d11ede16e0d4a64800
József Hajas <hayee4@gmail.com>
2023-02-06 22:22:16 +0100

    After middleware

--- server.js
+++ server.js
@@ -1,34 +1,73 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3500
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    //res.send("Hello world!")
-    //res.sendFile('./views/index.html', {root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+// custom middleware
+app.use(logger);
+// third party middleware
+const whiteList = [
+  "https://www.google.hu",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+// built-in middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
+app.get("^/$|/index(.html)?", (req, res) => {
+  //res.send("Hello world!")
+  //res.sendFile('./views/index.html', {root: __dirname})
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html')
-})
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
 
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('Attempted to load hello.html')
-    next()
-}, (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
 
 // Route handlers
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
