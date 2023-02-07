6f8af19b9cf91f8746794ea99f4b9f6a42647ae7
Hajas József <jozsef3.hajas@evosoft.com>
2023-02-07 17:28:29 +0100

    After MVC REST API

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,33 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const data = {};
+const employeesController = require('../../controllers/employeesController')
 
-data.employees = require("../../data/employees.json");
+router.route("/")
+  .get(employeesController.getAllEmployees)
+  .post(employeesController.createEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee)
 
-router
-  .route("/")
-  .get((req, res) => {
-    res.json(data.employees);
-  })
-  .post((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .delete((req, res) => {
-    res.json({ id: req.body.id });
-  });
-
-router.route("/:id").get((req, res) => {
-  res.json({ id: req.params.id });
-});
+router.route("/:id")
+  .get(employeesController.getEmployee);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
@@ -9,34 +10,15 @@ const PORT = process.env.PORT || 3500;
 // custom middleware
 app.use(logger);
 // third party middleware
-const whiteList = [
-  "https://www.google.hu",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
 // built-in middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// serve static files
 app.use('/', express.static(path.join(__dirname, "./public")));
-app.use('/subdir', express.static(path.join(__dirname, "./public")));
-
-// route
+// routes
 app.use('/', require('./routes/root'))
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
-
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
