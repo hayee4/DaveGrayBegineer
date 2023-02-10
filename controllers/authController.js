b7049b7ed20a27f210619ffed8fa9b8eadfc6ee1
Hajas József <jozsef3.hajas@evosoft.com>
2023-02-10 16:28:57 +0100

    The End

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,13 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
+const User = require('../model/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const fsPromises = require('fs').promises
-const path = require('path')
 
 const handleLogin = async (req, res) => {
     const {user, pwd} = req.body
     if (!user || !pwd) return res.status(400).json({'message': 'Username and password are required.'})
-    const foundUser = usersDB.users.find(
-        person => person.username === user
-    )
+    
+    const foundUser = await User.findOne({username: user}).exec();
+
     if (!foundUser) return res.sendStatus(401)
     // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password)
@@ -28,7 +22,7 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30s' }
+            { expiresIn: '180s' }
         )
         const refreshToken = jwt.sign(
             { "username": foundUser.username },
@@ -36,19 +30,14 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' }
         )
         // Saving refreshToken with current user
-        const otherUser = usersDB.users.filter(
-            person => person.username !== foundUser.username
-        )
-        const currentUser = {...foundUser, refreshToken }
-        usersDB.setUsers([...otherUser, currentUser])
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        )
+        foundUser.refreshToken = refreshToken
+        const result = await foundUser.save();
+        console.log(result)
+
         res.cookie('jwt', refreshToken, {
             httpOnly: true, 
             sameSite: 'None',
-            secure: true,
+            //secure: true,
             maxAge: 24*60*60*1000
         })
         res.json({accessToken})
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,8 +1,6 @@
 const User = require('../model/User')
 const bcrypt = require('bcrypt')
 
-
-
 const handleNewUser = async (req, res) => {
     const {user, pwd} = req.body
     if (!user || !pwd) return res.status(400).json({'message': 'Username and password are required.'})
